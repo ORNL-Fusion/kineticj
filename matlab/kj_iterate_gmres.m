61ea8c51b48e9440fa949c3cecd2d8e694f86365
David Green <david.lindsay.green@gmail.com>
2018-01-11 16:49:04 -0500

    Looks like the curl curl is correct, and that term2 is at least what AR produces, so WTF, is my AR not giving a solution that has LHS=RHS? Bah.

--- matlab/kj_iterate_gmres.m
+++ matlab/kj_iterate_gmres.m
@@ -84,6 +84,10 @@ RHS = -i * w * u0 * jA;
 
 [myLHS,LHS_t1,LHS_t2] = kj_LHS(E_init);
 
+jP_ar = ([sum(arS('jP_r'),3)',sum(arS('jP_t'),3)',sum(arS('jP_z'),3)']');
+
+LHS_t2_ar = w^2/c^2 .* ( E_init + i/(w*eps0).*jP_ar );
+
 res = myLHS;% - RHS;
 
 res_r = res(0*n+1:1*n);
@@ -104,6 +108,27 @@ ax3 = subplot(3,1,3);
 plot(ax3,rIn,real(res_z))
 hold on
 plot(ax3,rIn,imag(res_z))
+
+f6=figure();
+f6.Name = 'Residual terms : LHS_t1, LHS_t2, RHS';
+ax1 = subplot(3,1,1);
+hold on
+plot(ax1,rIn,real(LHS_t1(1:n)))
+plot(ax1,rIn,real(LHS_t2(1:n)))
+plot(ax1,rIn,real(RHS(1:n)))
+plot(ax1,rIn,real(LHS_t2_ar(1:n)))
+
+ax2 = subplot(3,1,2);
+hold on
+plot(ax2,rIn,real(LHS_t1(1+n:2*n)))
+plot(ax2,rIn,real(LHS_t2(1+n:2*n)))
+plot(ax2,rIn,real(RHS(1+n:2*n)))
+
+ax3 = subplot(3,1,3);
+hold on
+plot(ax3,rIn,real(LHS_t1(1+2*n:3*n)))
+plot(ax3,rIn,real(LHS_t2(1+2*n:3*n)))
+plot(ax3,rIn,real(RHS(1+2*n:3*n)))
 
 
 % Call GMRES (using the nested function handle defined below)
@@ -163,6 +188,14 @@ stat = 0;
         LHS_t1_t = zeros(1,n);
         LHS_t1_z = zeros(1,n);
         
+        LHS_t1_r_2 = zeros(1,n);
+        LHS_t1_t_2 = zeros(1,n);
+        LHS_t1_z_2 = zeros(1,n);
+        
+        LHS_t1_r_2_h = zeros(1,n-1);
+        LHS_t1_t_2_h = zeros(1,n-1);
+        LHS_t1_z_2_h = zeros(1,n-1);
+        
         LHS_t2_r = zeros(1,n);
         LHS_t2_t = zeros(1,n);
         LHS_t2_z = zeros(1,n);
@@ -173,63 +206,28 @@ stat = 0;
         
         h = rIn(2)-rIn(1);
         
-        for j=1+1:M-1
-            
-            r = rIn(j);
-            
-            % - curl curl E
-            %
-            % See the $KINETICJ/mathematica/kj_curl_curl_finite_difference file for the
-            % derivation of this code chunk.
-            
-            curlcurl_r=i.*r.^(-2).*((-1).*i.*(nPhi.^2+kz.^2.*r.^2).*Er(j)+nPhi.*Et( ...
-                j)+(1/2).*h.^(-1).*r.*((-1).*nPhi.*Et((-1)+j)+nPhi.*Et(1+j)+ ...
-                kz.*r.*((-1).*Ez((-1)+j)+Ez(1+j))));
-            
-            
-            curlcurl_t=r.^(-2).*((-1).*i.*nPhi.*Er(j)+Et(j)+(-1/2).*h.^(-2).*r.*( ...
-                2.*h.^2.*i.^2.*kz.^2.*r.*Et(j)+h.*((-1).*Et((-1)+j)+Et(1+j)) ...
-                +2.*r.*(Et((-1)+j)+(-2).*Et(j)+Et(1+j))+h.*i.*nPhi.*(Er((-1) ...
-                +j)+(-1).*Er(1+j)+(-2).*h.*i.*kz.*Ez(j))));
-            
-            
-            curlcurl_z=(1/2).*h.^(-2).*(h.*r.^(-2).*(i.*(kz.*r.*((-1).*r.*Er((-1)+ ...
-                j)+r.*Er(1+j)+2.*h.*(Er(j)+i.*nPhi.*Et(j)))+(-2).*h.*i.* ...
-                nPhi.^2.*Ez(j))+r.*(Ez((-1)+j)+(-1).*Ez(1+j)))+(-2).*(Ez(( ...
-                -1)+j)+(-2).*Ez(j)+Ez(1+j)));
-            
-            % - curl curl E (again to check - from my old RS notes)
-            
-            ...
-            
-            
-            % + w^2/c^2 * ( E + i/(w*eps_0)*Jp )
-            
-            term2_r = w^2/c^2 * ( Er(j) + i/(w*eps0) * jP_r(j) );
-            term2_t = w^2/c^2 * ( Et(j) + i/(w*eps0) * jP_t(j) );
-            term2_z = w^2/c^2 * ( Ez(j) + i/(w*eps0) * jP_z(j) );
-            
-            % LHS = - curl curl E + w^2/c^2 * ( E + i/(w*eps_0)*Jp )
-            
-            LHS_r(j) = curlcurl_r + term2_r;
-            LHS_t(j) = curlcurl_t + term2_t;
-            LHS_z(j) = curlcurl_z + term2_z;
-            
-            LHS_t1_r(j) = curlcurl_r;
-            LHS_t1_t(j) = curlcurl_t;
-            LHS_t1_z(j) = curlcurl_z;
-            
-            LHS_t2_r(j) = term2_r;
-            LHS_t2_t(j) = term2_t;
-            LHS_t2_z(j) = term2_z;
-            
-        end
+        % term1 = -curlxcurl(E)
+        
+        t1_r = -(i*nPhi./rIn.^2.*gradient(rIn.*Et,h) + nPhi^2./rIn.^2.*Er + kz^2*Er + i*kz.*gradient(Ez,h));
+        t1_t = -(-kz*nPhi./rIn.*Ez + kz^2.*Et - gradient(gradient(rIn.*Et,h)./rIn,h) + i*nPhi.*gradient(Er./rIn,h));
+        t1_z = -(i*kz./rIn.*gradient(rIn.*Er,h) - 1./rIn.*gradient(rIn.*gradient(Ez,h),h) + nPhi^2./rIn.^2.*Ez - nPhi*kz./rIn.*Et);
+        
+        % term2 = +w^2/c^2 * ( E + i/(w*e0) * jP )
+        
+        t2_r = w^2/c^2 .* ( Er + i/(w*eps0) .* jP_r );
+        t2_t = w^2/c^2 .* ( Et + i/(w*eps0) .* jP_t );
+        t2_z = w^2/c^2 .* ( Ez + i/(w*eps0) .* jP_z );
+        
+        LHS_r = t1_r' + t2_r';
+        LHS_t = t1_t' + t2_t';
+        LHS_z = t1_z' + t2_z';
         
         % Return A*x vector for GMRES
         
         LHS = [LHS_r,LHS_t,LHS_z]';
-        LHS_t1 = [LHS_t1_r,LHS_t1_t,LHS_t1_z]';
-        LHS_t2 = [LHS_t2_r,LHS_t2_t,LHS_t2_z]';
+        
+        LHS_t1 = [t1_r',t1_t',t1_z']';
+        LHS_t2 = [t2_r',t2_t',t2_z']';
         
     end
 
